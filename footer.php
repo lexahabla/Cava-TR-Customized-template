9d88fbd9753b6fab3da6b39726f0e2db4eea358f
Lexahabla <56838787+lexahabla@users.noreply.github.com>
2021-10-09 12:21:52 +0200

    header and footer files php

---
+++ footer.php
@@ -0,0 +1,9 @@
+
+    <!--footer-->
+    <footer class="container-fluid py-4 text-center text-light bg-dark">
+      <h5>Cava Tercera Republica</h5>
+    </footer>
+    <!--footer-->
+    <?php wp_footer(); ?>
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+   <?php wp_head(); ?>
+    <title>Cava Tercera Republica</title>
+
+  </head>
+  <body>
+  
+<div class="row">
+
+<div class="nav" id="fondomenu">
+					<!-- <div class="idiomas">
+						<div class="lang" id="anular">
+							<a href="#">
+							<img src="img/idiomas.png" id="langImg" onclick="idiomasIn();langout();" alt="Mostra els idiomes del Web Cava Tercera República">
+							</a>
+						</div>
+						<div class="lang2" id="anular2">
+							<a href="#">
+							<img src="img/idiomas2.png" id="lang2Img" onclick="idiomasOut();langin();" alt="Amaga els idiomas del Web Cava Tercera República">
+							</a>
+						</div>
+						<div id="cat">
+							<a href=""><img id="catImg"  src="img/catalan.png" alt="Web Cava Tercera República en Català" onmouseover="idiomasIn();" onmouseout="idiomasOut();langin();"></a>
+						</div>
+						<div id="cast">
+							<a href="Cast/index.html"><img id="castImg"  src="img/castellano.png" alt="Web Cava Tercera República en Castellano" onmouseover="idiomasIn();" onmouseout="idiomasOut();langin();"></a>
+						</div>
+					
+					</div> -->
+    
+    <nav class="navbar navbar-expand-lg bg-dark" id="menuResp">
+        <button id="ham" class="navbar-toggler fixed-top" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div id="menuCaja">
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            
+                <ul class="navbar-nav text-center ">
+                    <li class="nav-item">
+                    <a class="nav-link text-secondary" href="inici.html"> <span class="selected"><b>INICI</b></span> </a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link text-secondary" href="republica.html"><b>REPÚBLICA</b></a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link text-secondary	" href="origen.html"><b>ORIGEN</b></a>
+                    </li>
+                    <div id="xapa">
+                    <a class="navbar-brand"  href="index.html"><img src="http://localhost/ficheros/WORDPRESS/cava_tercera_republica/wp-content/uploads/2021/10/xapa.png" alt="Xapa del cava Tercera República"></a>
+                    </div>
+                    <li class="nav-item">
+                    <a class="nav-link text-secondary" href="cava.html"><b>CAVA</b></a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link text-secondary" href="tast.html"><b>TAST</b></a>
+                    </li>
+                     <li class="nav-item">
+                    <a class="nav-link text-secondary" href="botiga.html"><b>BOTIGA</b></a>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    </nav>
+</div>
+</div>
+
+    <!--menu-->
+
+    <!--blog-->
+    <div class="container">
+
+      <h2 class="my-3 text-center text-primary">Tauler</h2>
+      <hr>
