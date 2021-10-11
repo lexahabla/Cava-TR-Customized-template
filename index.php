c10b46c5bf5a0a57047075845c1b1b89c8b8b98c
Lexahabla <56838787+lexahabla@users.noreply.github.com>
2021-10-11 11:48:24 +0200

    pagination with folder template-parts - content-pagiination

--- index.php
+++ index.php
@@ -36,15 +36,7 @@
        
           <!--paginacion-->
           <div class="card-body">
-            <nav aria-label="Page navigation example">
-              <ul class="pagination">
-                <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                <li class="page-item"><a class="page-link" href="#">Next</a></li>
-              </ul>
-            </nav>
+           <?php get_template_part('template-parts/content', 'pagination'); ?>
           </div>
 
           <!--paginacion-->
